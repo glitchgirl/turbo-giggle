3bafe604bb1f029ab96cc518f18199f269320ad0
glitchgirl <londonspleen@gmail.com>
2019-09-11 10:01:28 -0400

    day2 work

---
+++ day2.js
@@ -0,0 +1,15 @@
+//day 2 exercise 1
+var stringOne = "hello";
+var stringTwo = "world";
+
+console.log(stringOne + " " + stringTwo);
+
+//day 2 exercise 3
+var stringThree = "bagel";
+var numberOne = 3;
+
+console.log(stringThree * numberOne);
+
+//bonus
+
+console.log(2 + false);
